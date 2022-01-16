75ccb8a6c41f3c55766dc8e480e4db19c5a36754
Yohei Kokubo <yhei.register@gmail.com>
2022-01-16 21:53:46 +0900

    セキュリティグループの作成

--- network.tf
+++ network.tf
@@ -134,7 +134,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_route" "public_rt_igw_r" {
-  route_table_id                     = aws_route_table.public_rt.id
+  route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id                         = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.igw.id
 }
