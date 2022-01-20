f1b59345a610c5dc5db5ae1193d842e742887479
Yohei Kokubo <yhei.register@gmail.com>
2022-01-21 08:49:35 +0900

    RDS - ランダム文字列の作成

--- rds.tf
+++ rds.tf
@@ -41,3 +41,11 @@ resource "aws_db_subnet_group" "mysql_standalone_subnetgroup" {
     Env     = var.environment
   }
 }
+
+# -----------------------
+# RDS instance
+# -----------------------
+resource "random_string" "db_password" {
+  length  = 16
+  special = false
+}
