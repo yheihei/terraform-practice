8e7aba6e160b919f36886339578cfdce3a1085ef
Yohei Kokubo <yhei.register@gmail.com>
2022-01-24 08:21:01 +0900

    キーペアの作成

---
+++ appserver.tf
@@ -0,0 +1,13 @@
+# ----------------------------------
+# key pair
+# ----------------------------------
+resource "aws_key_pair" "keypair" {
+  key_name   = "${var.project}-${var.environment}-keypair"
+  public_key = file("./src/tastylog-dev-keypair.pub")
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-keypair"
+    Project = var.project
+    Env     = var.environment
+  }
+}
