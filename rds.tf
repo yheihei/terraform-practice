f50a8183be30f22fd73c5da6a3b1f00b8465b248
Yohei Kokubo <yhei.register@gmail.com>
2022-01-23 10:05:31 +0900

    RDS - RDSの作成

--- rds.tf
+++ rds.tf
@@ -49,3 +49,46 @@ resource "random_string" "db_password" {
   length  = 16
   special = false
 }
+resource "aws_db_instance" "mysql_standalone" {
+  engine         = "mysql"
+  engine_version = "8.0.20"
+
+  identifier = "${var.project}-${var.environment}-mysql-standalone"
+
+  username = "admin"
+  password = random_string.db_password.result
+
+  instance_class = "db.t2.micro"
+
+  allocated_storage     = 20
+  max_allocated_storage = 50
+  storage_type          = "gp2"
+  storage_encrypted     = false
+
+  multi_az               = false
+  availability_zone      = "ap-northeast-1a"
+  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  publicly_accessible    = false
+  port                   = 3306
+
+  name                 = "tastylog"
+  parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
+  option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
+
+  backup_window              = "04:00-05:00"         # バックアップを実施する時間帯
+  backup_retention_period    = 7                     # バックアップの保持日数
+  maintenance_window         = "Mon:05:00-Mon:08:00" # 自動更新などのメンテナンスを行う時間帯。バックアップを先にしておくこと
+  auto_minor_version_upgrade = false
+
+  deletion_protection = true
+  skip_final_snapshot = false # インスタンス削除時にスナップショットを取るかどうか
+
+  apply_immediately = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-mysql-standalone"
+    Project = var.project
+    Env     = var.environment
+  }
+}
