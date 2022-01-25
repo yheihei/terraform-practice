50ddab12c73619ac7e24aea32ae3c6c2c33f0746
Yohei Kokubo <yhei.register@gmail.com>
2022-01-26 08:45:46 +0900

    IAMロールの作成

--- iam.tf
+++ iam.tf
@@ -1,3 +1,11 @@
+# ---------------------------
+# IAM Role
+# ---------------------------
+resource "aws_iam_role" "app_iam_role" {
+  name = "${var.project}-${var.environment}-app-iam-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
 data "aws_iam_policy_document" "ec2_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
