ad2ec2561626f8a041999fc2d903137f47b0a16a
Yohei Kokubo <yhei.register@gmail.com>
2022-01-30 06:59:55 +0900

    ALBの作成

---
+++ elb.tf
@@ -0,0 +1,15 @@
+# ----------------------------------
+# ALB
+# ----------------------------------
+resource "aws_lb" "alb" {
+  name               = "${var.project}-${var.environment}-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups = [
+    aws_security_group.web_sg.id
+  ]
+  subnets = [
+    aws_subnet.public_subnet_1a.id,
+    aws_subnet.public_subnet_1c.id
+  ]
+}
