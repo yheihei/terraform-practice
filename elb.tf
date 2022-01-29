9263bdc3b80007465687c41a242981e6493545ab
Yohei Kokubo <yhei.register@gmail.com>
2022-01-30 07:12:25 +0900

    ターゲットグループ作成

--- elb.tf
+++ elb.tf
@@ -13,3 +13,24 @@ resource "aws_lb" "alb" {
     aws_subnet.public_subnet_1c.id
   ]
 }
+
+# ----------------------------------
+# target group
+# ----------------------------------
+resource "aws_lb_target_group" "alb_target_group" {
+  name     = "${var.project}-${var.environment}-app-tg"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-tg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_lb_target_group_attachment" "instance" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = aws_instance.app_server.id
+}
