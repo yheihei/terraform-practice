7fda9a4cfdfd21888d02bf0761945b4140173e44
Yohei Kokubo <yhei.register@gmail.com>
2022-01-30 11:14:06 +0900

    リスナーの作成

--- elb.tf
+++ elb.tf
@@ -14,6 +14,17 @@ resource "aws_lb" "alb" {
   ]
 }
 
+resource "aws_lb_listener" "alb_listener_http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
+
 # ----------------------------------
 # target group
 # ----------------------------------
