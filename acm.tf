3c3524bcc8754a5bc00067a8336a218802663373
Yohei Kokubo <yhei.register@gmail.com>
2022-01-30 21:52:04 +0900

    証明書の発行

---
+++ acm.tf
@@ -0,0 +1,22 @@
+# ----------------------------------
+# Certificate
+# ----------------------------------
+# for tokyo region
+resource "aws_acm_certificate" "tokyo_cert" {
+  domain_name       = "*.${var.domain}"
+  validation_method = "DNS"
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-wildcard-sslcert"
+    Project = var.project
+    Env     = var.environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [
+    aws_route53_zone.route53_zone
+  ]
+}
