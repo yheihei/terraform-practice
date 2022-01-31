ca7b37d889c85d902fe87ea999fc15e1310b7339
Yohei Kokubo <yhei.register@gmail.com>
2022-02-01 05:57:37 +0900

    デプロイ用のプライベートバケット作成

--- s3.tf
+++ s3.tf
@@ -44,3 +44,42 @@ data "aws_iam_policy_document" "s3_static_bucket" {
     }
   }
 }
+
+# ----------------------------------
+# S3 deploy bucket
+# ----------------------------------
+resource "aws_s3_bucket" "s3_deploy_bucket" {
+  bucket = "${var.project}-${var.environment}-deploy-bucket-${random_string.s3_unique_key.result}"
+
+  versioning {
+    enabled = false
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_deploy_bucket" {
+  bucket                  = aws_s3_bucket.s3_deploy_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  depends_on = [
+    aws_s3_bucket_policy.s3_deploy_bucket
+  ]
+}
+
+resource "aws_s3_bucket_policy" "s3_deploy_bucket" {
+  bucket = aws_s3_bucket.s3_deploy_bucket.id
+  policy = data.aws_iam_policy_document.s3_deploy_bucket.json
+}
+
+data "aws_iam_policy_document" "s3_deploy_bucket" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.s3_deploy_bucket.arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.app_iam_role.arn]
+    }
+  }
+}
