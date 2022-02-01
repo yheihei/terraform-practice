81d2125967423688100d5586cb70278949c44383
Yohei Kokubo <yhei.register@gmail.com>
2022-02-01 12:14:37 +0900

    CloudFrontの作成(S3オリジン)

--- cloudfront.tf
+++ cloudfront.tf
@@ -19,6 +19,15 @@ resource "aws_cloudfront_distribution" "cf" {
     }
   }
 
+  origin {
+    domain_name = aws_s3_bucket.s3_static_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.s3_static_bucket.id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.cf_s3_origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
   default_cache_behavior {
     allowed_methods = ["GET", "HEAD"]
     cached_methods  = ["GET", "HEAD"]
@@ -37,6 +46,27 @@ resource "aws_cloudfront_distribution" "cf" {
     max_ttl                = 0
   }
 
+  ordered_cache_behavior {
+    path_pattern     = "/public/*"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.s3_static_bucket.id
+
+    forwarded_values {
+      query_string = false
+      headers      = []
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
@@ -52,14 +82,18 @@ resource "aws_cloudfront_distribution" "cf" {
   }
 }
 
+resource "aws_cloudfront_origin_access_identity" "cf_s3_origin_access_identity" {
+  comment = "S3 static bucket access identity"
+}
+
 resource "aws_route53_record" "route53_cloudfront" {
   zone_id = aws_route53_zone.route53_zone.id
   name    = "dev.${var.domain}"
   type    = "A"
 
   alias {
-    name                      = aws_cloudfront_distribution.cf.domain_name
-    zone_id                   = aws_cloudfront_distribution.cf.hosted_zone_id
+    name                   = aws_cloudfront_distribution.cf.domain_name
+    zone_id                = aws_cloudfront_distribution.cf.hosted_zone_id
     evaluate_target_health = true
   }
 }
--- s3.tf
+++ s3.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_public_access_block" "s3_static_bucket" {
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
-  restrict_public_buckets = false
+  restrict_public_buckets = true
   depends_on = [
     aws_s3_bucket_policy.s3_static_bucket
   ]
@@ -39,8 +39,8 @@ data "aws_iam_policy_document" "s3_static_bucket" {
     actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.s3_static_bucket.arn}/*"]
     principals {
-      type        = "*"
-      identifiers = ["*"]
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.cf_s3_origin_access_identity.iam_arn]
     }
   }
 }
